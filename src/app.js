27dd873e73c0acc79ce781b3cd620f4c7a576fdc
DamianGallardo <damiangspr@gmail.com>
2024-07-26 20:37:09 -0600

    Fix Files

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ import express from 'express';
 import moviesRouter from './routes/movies.routes.js';
 import authRouter from './routes/auth.routes.js';
 import cors from 'cors';
+import { config } from 'dotenv';
+
+config();
 
 const app = express();
 // Configura CORS
 app.use(cors({
-    origin: 'http://localhost:4200' // Reemplaza con el origen que necesitas
+    origin: process.env.PORT_ANGULAR // URL de Angular
   }));
 app.use(moviesRouter);
 app.use(authRouter);
